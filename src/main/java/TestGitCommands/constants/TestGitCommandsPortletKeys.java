3dd7e4cefd39366729b0af4ac937a2b93f342216
unknown <vinothsquare110@gmail.com>
2022-01-27 16:23:29 +0530

    First commit of Module project

---
+++ src/main/java/TestGitCommands/constants/TestGitCommandsPortletKeys.java
@@ -0,0 +1,11 @@
+package TestGitCommands.constants;
+
+/**
+ * @author vinoth.l
+ */
+public class TestGitCommandsPortletKeys {
+
+	public static final String TESTGITCOMMANDS =
+		"TestGitCommands_TestGitCommandsPortlet";
+
+}
---
+++ src/main/java/TestGitCommands/portlet/TestGitCommandsPortlet.java
@@ -0,0 +1,30 @@
+package TestGitCommands.portlet;
+
+import TestGitCommands.constants.TestGitCommandsPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author vinoth.l
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=TestGitCommands",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + TestGitCommandsPortletKeys.TESTGITCOMMANDS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class TestGitCommandsPortlet extends MVCPortlet {
+}
